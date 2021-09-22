3f8d2c90ca9f4a121ac1ba21ddf8983048caf8c4
MarcoPorcaro1987 <marcoporcaro87@gmail.com>
2021-09-22 11:28:58 +0100

    Aggiunta Milestone 2

--- js/main.js
+++ js/main.js
@@ -41,7 +41,7 @@ var app = new Vue({
 					{
 						date: '20/03/2020 16:35:00',
 						message: 'Mi piacerebbe ma devo andare a fare la spesa.',
-						status: 'received'
+						status: 'sent'
 					}
 				],
 			},
